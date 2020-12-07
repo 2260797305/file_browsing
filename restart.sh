9878c71f401f20dbe07935e43fd64b9950b016fc
2260797305 <31440935+2260797305@users.noreply.github.com>
2020-12-07 10:44:02 +0800

    Update restart.sh
    start up 修改

--- restart.sh
+++ restart.sh
@@ -1,20 +1,31 @@
-v="0"
-echo "kill old process"
-ps -efww|grep -w 'http.js' | grep -v "grep"
+#!/bin/bash
+#sleep 1
+
+cd /home/k-yuki
+
+#echo "kill old process"
+process_name='test.js'
+ps -efww|grep -w $process_name | grep -v "grep"
 while (($? == 0))
-do 
-	ps -efww|grep -w 'http.js'|grep -v grep|cut -c 11-15 | xargs kill -9
-	# echo "kill " "$(pid)"
-	# kill -9 $(pid)
-	# echo "kill " $(v)
-	ps -efww|grep -w 'http.js' | grep -v "grep"
+do
+        ps -efww|grep -w $process_name |grep -v grep|cut -c 11-15 | xargs kill -9
+        # echo "kill " "$(pid)"
+        # kill -9 $(pid)
+        # echo "kill " $(v)
+        ps -efww|grep -w $process_name | grep -v "grep"
 done
 
-echo "start process"
+#echo "start process"
 a=`date "+%Y-%m-%d_%H_%M_%S"`
-echo  log file: ./log/$a.log
-nohup node http.js  1>./log/$a.log 2>&1 &
+#echo  log file: ./log/$a.log
 
+nohup node $process_name 1>./log/$a.log 2>&1 &
+sync
+#echo "startup over"
 
+#sleep 5
 
+#nohup /home/k-yuki/restart.sh 1>/dev/null  2>&1 &
+#nohup /home/k-yuki/run_test.sh 1>/dev/null  2>&1 &
+#sync
 
